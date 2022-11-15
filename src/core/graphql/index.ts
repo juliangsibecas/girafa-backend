cef7f10fb5ab601459ba14c8d41cf295c5e91067
Julian Gomez Sibecas <juliangsibecas@gmail.com>
2022-11-14 22:56:06 -0300

    Remove playground on prod

--- src/core/graphql/index.ts
+++ src/core/graphql/index.ts
@@ -1,16 +1,22 @@
 import { join } from 'path';
-import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver } from '@nestjs/apollo';
 import { handleError } from './utils';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { appConfig } from 'src/app';
+import { Environment } from 'src/common/types';
 
-export const gqlModuleOptions: GraphQLModule['options'] = {
+export const gqlModuleOptions = {
+  imports: [ConfigModule.forFeature(appConfig)],
+  inject: [ConfigService],
   driver: ApolloDriver,
-  autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-  sortSchema: true,
-  playground: true,
-  debug: false,
-  formatError: handleError,
-  context: ({ req, res }) => ({ req, res }),
+  useFactory: (config: ConfigService) => ({
+    autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    sortSchema: true,
+    playground: config.get('app.env') === Environment.DEVELOPMENT,
+    debug: false,
+    formatError: handleError,
+    context: ({ req, res }) => ({ req, res }),
+  }),
 };
 
 export * from './errors';
